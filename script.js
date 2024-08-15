975bbd46e7341cb376b4a002f17f92fb3215392b
solomontudu <tukintudu@gmail.com>
2024-08-15 20:47:55 +0530

    added event listner for form submit, prevenDefault(), pulled out the values from the even object from the form submit event

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+let countdownTitle = "";
+let countdownDate = "";
+
 // set date input min with today's date
 const today = new Date().toISOString().split("T")[0];
 
 dateEl.setAttribute("min", today);
-console.log(today);
+
+// take values from form input
+function updateCountDown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+
+  console.log(countdownTitle, countdownDate);
+}
+
+// event listners
+countdownForm.addEventListener("submit", updateCountDown);
