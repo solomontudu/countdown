660e33673925f1e1bb110d4a343ba18453cfdb0a
solomontudu <tukintudu@gmail.com>
2024-08-22 14:40:03 +0530

    populated the timer elements, and updating the UI

--- script.js
+++ script.js
@@ -2,13 +2,47 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+const countdownEl = document.getElementById("countdown");
+const countdownElTitle = document.getElementById("countdown-title");
+const countdownBtn = document.getElementById("countdown-button");
+const timeElements = document.querySelectorAll("span");
+
 let countdownTitle = "";
 let countdownDate = "";
+let countdownValue = Date;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 // set date input min with today's date
 const today = new Date().toISOString().split("T")[0];
 
 dateEl.setAttribute("min", today);
+
+// populate countdown / complete UI
+function updateDOM() {
+  const now = new Date().getTime();
+  const distance = countdownValue - now;
+
+  const days = Math.floor(distance / day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minute);
+  const seconds = Math.floor((distance % minute) / second);
+
+  // hide input
+  inputContainer.hidden = true;
+  // show countdown
+  countdownEl.hidden = false;
+
+  // populating countdown
+  countdownElTitle.textContent = `${countdownTitle}`;
+  timeElements[0].textContent = `${days}`;
+  timeElements[1].textContent = `${hours}`;
+  timeElements[2].textContent = `${minutes}`;
+  timeElements[3].textContent = `${seconds}`;
+}
 
 // take values from form input
 function updateCountDown(e) {
@@ -16,7 +50,9 @@ function updateCountDown(e) {
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.srcElement[1].value;
 
-  console.log(countdownTitle, countdownDate);
+  // get number version of current date, updateDOM
+  countdownValue = new Date(countdownDate).getTime();
+  updateDOM();
 }
 
 // event listners
